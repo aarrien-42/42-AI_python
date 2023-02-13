5e2f79abc140b6491390dea06e9e7e2e76caf704
Andoni Arrien Martin <aarrien-@c4r6s6.42urduliz.com>
2023-02-13 15:04:39 +0100

    ex01 module01

---
+++ module01/ex01/game.py
@@ -0,0 +1,24 @@
+class	GotCharacter:
+	def	__init__(self, fn, ia):
+		self.first_name = fn
+		self.is_alive = ia
+	first_name=""
+	is_alive=True
+class	Stark(GotCharacter):
+	"""A class representing the Stark family. Or when bad things happen to good people."""
+	def	__init__(self, first_name=None, is_alive=True):
+		super().__init__(first_name, is_alive)
+		self.family_name="Stark"
+		self.house_words="Winter is Coming"
+	def	print_house_words(self):
+		print(self.house_words)
+	def	die(self):
+		self.is_alive = False
+
+arya = Stark("Arya")
+arya.print_house_words()
+print(arya.is_alive)
+arya.die()
+print(arya.is_alive)
+print(dict(arya.__dict__))
+print(arya.__doc__)
