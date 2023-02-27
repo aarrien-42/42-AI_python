1f5eb46eb3ef1de0b0f7c9ea1dd6adef9de9b89d
Andoni Arrien Martin <aarrien-@c4r5s6.42urduliz.com>
2023-02-27 16:55:19 +0100

    ex10 fixed

--- module00/ex10/loading.py
+++ module00/ex10/loading.py
@@ -1,13 +1,15 @@
 from time import sleep
 from tqdm import tqdm
 
-def ft_progress(listy):
-	return 0
+def ft_progress(lst):
+	for i in tqdm(lst):
+		yield i
 
-listy = range(1000)
-ret = 0
-for elem in ft_progress(listy):
-	ret += (elem + 3) % 5
-	sleep(0.01)
-print()
-print(ret)
+if __name__ == '__main__':
+	listy = range(1000)
+	ret = 0
+	for elem in ft_progress(listy):
+		ret += (elem + 3) % 5
+		sleep(0.01)
+	print()
+	print(ret)
